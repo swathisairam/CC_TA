35978ee17f012c71db47379c62a7f031794d57d5
swathisairam <swathisairam15@gmail.com>
2025-03-13 15:19:57 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,10 +1,7 @@
-//File: hello.cpp
 #include <iostream>
 using namespace std;
 
-int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
-  return 0;
+int main() {
+    cout << "Hello, Jenkins! Webhook triggered successfully!" << endl;
+    return 0;
 }
